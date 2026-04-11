12a77803f339c4294af07ad653bb46fc2f88dcc8
Maciej Kowalski <maciej.ko1444@gmail.com>
2026-04-11 14:23:49 +0200

    first commit with nvim

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,8 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>sG', function()
+	builtin.grep_string({search = vim.fn.input("Grep > ")})
+end, { desc = 'Telescope grep string' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
