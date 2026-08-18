192afc1e9c7f4c5efab6a8c8d569dd5a317128e1
Maciej Kowalski <maciej.ko1444@gmail.com>
2026-08-18 21:25:39 +0200

    change colorscheme to catppuccin in nvim

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,19 +1,19 @@
 require("rose-pine").setup({ styles = { italic = false }, highlight_groups = { Comment = { italic = true } } })
 require("catppuccin").setup({
-	flavour = "mocha",
-	transparent_background = false,
-	styles = { italic = false },
-	integrations = {
-		blink_cmp = true,
-		gitsigns = true,
-		lsp_trouble = true,
-		mason = true,
-		snacks = true,
-		treesitter = true,
-		which_key = true,
-		bufferline = true,
-		lualine = true,
-	},
+    flavour = "mocha",
+    transparent_background = false,
+    styles = { italic = false },
+    integrations = {
+        blink_cmp = true,
+        gitsigns = true,
+        lsp_trouble = true,
+        mason = true,
+        snacks = true,
+        treesitter = true,
+        which_key = true,
+        bufferline = true,
+        lualine = true,
+    },
 })
 
-vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("catppuccin")
